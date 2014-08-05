f3776833669c2f70a1481490195505a04adbd2f3
Nattgew <xty2acc@gmail.com>
2014-08-05 09:37:04 -0700

    Update settings.c

--- settings.c
+++ settings.c
@@ -4,6 +4,21 @@ fr = fopen ("login.txt", "rt");
 
 if( fr == NULL) perror("Error opening settings file.\n");
 
-fscanf(fr, "%s,%s", &PID, &Ppass);
+fscanf(fr, "%s %s %s %s %s", &PID, &Ppass, &pirepurl, &acarsurl, &fdurl);
 
 fclose(fr);
+
+
+Example file:
+
+PID
+pass
+http://www.swavirtual.com/wn/xacars/pirep.php
+http://www.swavirtual.com/wn/xacars/liveacars.php
+http://www.swavirtual.com/wn/xacars/flightdata.php
+
+//xactesting
+//xactestingpass
+//http://www.xacars.net/acars/pirep.php
+//http://www.xacars.net/acars/liveacars.php
+//http://www.xacars.net/acars/flightdata.php
