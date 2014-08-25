a707ba915c23acd08333bc5b3b427bd1cce7eb52
Nattgew <xty2acc@gmail.com>
2014-08-25 09:08:04 -0700

    Update xdmg.py

--- xdmg.py
+++ xdmg.py
@@ -15,14 +15,16 @@ class PythonInterface:
 		self.OAT_ref=XPLMFindDataRef("sim/weather/temperature_ambient_c")
 		self.RPM_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_N2_")
 		self.CHT_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_CHT_c")
+		self.ITT_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_ITT_c")
+		self.EGT_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_EGT_c")
 		self.mix_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_mixt")
 		self.flightTime_ref=XPLMFindDataRef("sim/time/total_flight_time_sec")
 		self.num_eng_ref=XPLMFindDataRef("sim/aircraft/engine/acf_num_engines")
 		self.alt_ref=XPLMFindDataRef("sim/flightmodel/position/y_agl")
+		self.eng_type_ref=XPLMFindDataRef("sim/aircraft/prop/acf_en_type")
 		self.prop_type_ref=XPLMFindDataRef("sim/aircraft/prop/acf_prop_type")
-		self.acf_fuel_ref=XPLMFindDataRef("sim/aircraft/weight/acf_m_fuel_tot") #flt
-		self.m_fuel_ref=XPLMFindDataRef("sim/flightmodel/weight/m_fuel_total") #flt
-		self.eng_type_ref=XPLMFindDataRef("sim/aircraft/prop/acf_en_type") #int[8]
+		self.r_EGT_ref=XPLMFindDataRef("sim/aircraft/limits/red_lo_EGT")
+		self.r_ITT_ref=XPLMFindDataRef("sim/aircraft/limits/red_lo_ITT")
 		
 		self.started=0
 		self.gWindow=0
@@ -41,8 +43,8 @@ class PythonInterface:
 		self.runtime=0
 		self.chtDamage=0
 		self.mixtureDamage=0
-		self.prop_type=[]
 		self.eng_type=[]
+		self.prop_type=[]
 
 		self.gameLoopCB=self.gameLoopCallback
 		self.DrawWindowCB=self.DrawWindowCallback
@@ -53,7 +55,7 @@ class PythonInterface:
 		self.MyHotKeyCB=self.MyHotKeyCallback
 		self.gHotKey=XPLMRegisterHotKey(self, XPLM_VK_D, xplm_DownFlag+xplm_ShiftFlag+xplm_ControlFlag, "Shows FSE damage info", self.MyHotKeyCB, 0)
 		self.MyHotKeyCB2=self.MyHotKeyCallback2
-		self.gHotKey2=XPLMRegisterHotKey(self, XPLM_VK_M, xplm_DownFlag+xplm_ShiftFlag+xplm_ControlFlag, "Sets mixture below FSE threshold", self.MyHotKeyCB2, 0)
+		self.gHotKey2=XPLMRegisterHotKey(self, XPLM_VK_M, xplm_DownFlag+xplm_ShiftFlag+xplm_ControlFlag, "Sets mixture below FSE damage threshold", self.MyHotKeyCB2, 0)
 		
 		return self.Name, self.Sig, self.Desc
 
@@ -67,8 +69,10 @@ class PythonInterface:
 		if self.started == 0 :
 			self.started=1
 			self.num_eng=XPLMGetDatai(self.num_eng_ref)
-			XPLMGetDatavi(self.prop_type_ref, self.prop_type, 0, self.num_eng)
 			XPLMGetDatavi(self.eng_type_ref, self.eng_type, 0, self.num_eng)
+			XPLMGetDatavi(self.prop_type_ref, self.prop_type, 0, self.num_eng)
+			self.r_EGT=XPLMGetDataf(self.r_EGT_ref)
+			self.r_ITT=XPLMGetDataf(self.r_ITT_ref)
 			self.defaultcht=XPLMGetDataf(self.OAT_ref)
 			XPLMRegisterFlightLoopCallback(self, self.gameLoopCB, 1, 0)
 		else:
@@ -99,6 +103,7 @@ class PythonInterface:
 
 	def XPluginStop(self):
 		XPLMUnregisterHotKey(self, self.gHotKey)
+		XPLMUnregisterHotKey(self, self.gHotKey2)
 		XPLMUnregisterFlightLoopCallback(self, self.gameLoopCB, 0)
 		self.closeEventWindow()
 		pass
@@ -127,31 +132,60 @@ class PythonInterface:
 	def gameLoopCallback(self, inElapsedSinceLastCall, elapsedSim, counter, refcon):
 
 		if (XPLMGetDataf(self.flightTime_ref) > 3.0):
+			# rpms=[]
+			# XPLMGetDatavf(self.RPM_ref, rpms, 0, self.num_eng)
+			# if rpms[0] > 0:
+				# self.runtime+=1
+			# #Let's do some damage
+			# chts=[]
+			# XPLMGetDatavf(self.CHT_ref, chts, 0, self.num_eng)
+			# if self.defaultcht>0:
+				# _diff=abs(chts[0]-self.defaultcht)
+				# if _diff>0:
+					# #COOL THE ENGINES
+					# self.chtDamage+=_diff
+			# self.defaultcht=chts[0]
+			# mixes=[]
+			# XPLMGetDatavf(self.mix_ref, mixes, 0, self.num_eng)
+			# mixes*=100
+			# if (mixes[0] > 0.95 and XPLMGetDataf(self.alt_ref) > 1000):
+				# #SMOKIN'
+				# self.mixtureDamage += 1
+			
 			rpms=[]
 			XPLMGetDatavf(self.RPM_ref, rpms, 0, self.num_eng)
-			if rpms[0] > 0:
-				self.runtime+=1
-			#Let's do some damage
-			chts=[]
-			XPLMGetDatavf(self.CHT_ref, chts, 0, self.num_eng)
-			if self.defaultcht>0:
-				_diff=abs(chts[0]-self.defaultcht)
-				if _diff>0:
-					#COOL THE ENGINES
-					self.chtDamage+=_diff
-			self.defaultcht=chts[0]
-			mixes=[]
-			XPLMGetDatavf(self.mix_ref, mixes, 0, self.num_eng)
-			mixes*=100
-			if (mixes[0] > 0.95 and XPLMGetDataf(self.alt_ref) > 1000):
-				#SMOKIN'
-				self.mixtureDamage += 1
-			m_fuel=XPLMGetDataf(self.m_fuel_ref)
-			a_fuel=XPLMGetDataf(self.acf_fuel_ref)
-			self.msg1="Run: "+str(self.runtime)+" Type: "+str(self.prop_type[0])
-			self.msg2="CHT: "+str(round(chts[0],2))+" dmg: "+str(round(self.chtDamage,2))
+			engineType=self.eng_type[0]
+			if rpms[0]>0:
+				self.runtime+=sec
+			if engineType==2 or engineType==8:
+				itts=[]
+				XPLMGetDatavf(self.ITT_ref, itts, 0, self.num_eng)
+				if self.r_ITT>0 and itts[0]>self.r_ITT:
+					self.chtDamage += sec
+				if altitude < 1000:
+					self.mixtureDamage += sec
+				self.msg2="ITT: "+str(round(itts[0]))+"/"+str(round(self.r_ITT))+" dmg: "+str(round(self.chtDamage,2))
+			elif engineType==4 or engineType==5:
+				egts=[]
+				XPLMGetDatavf(self.EGT_ref, egts, 0, self.num_eng)
+				if self.r_EGT>0 and egts[0]>self.r_EGT:
+					self.chtDamage += sec
+				if altitude < 1000:
+					self.mixtureDamage += sec
+				self.msg2="EGT: "+str(round(egts[0]))+"/"+str(round(self.r_EGT))+" dmg: "+str(round(self.chtDamage,2))
+			else:
+				if self.defaultcht>0:
+					_diff=abs(cht-self.defaultcht)/float(sec)
+					if _diff>0:
+						self.chtDamage+=_diff
+				self.defaultcht=cht
+				if (mix > 95 and altitude > 1000):
+					self.mixtureDamage += sec
+				self.msg2="CHT: "+str(round(chts[0],2))+" dmg: "+str(round(self.chtDamage,2))
+
+			self.msg1="Run: "+str(self.runtime)+" RPM: "+str(round(rpms[0]))
 			self.msg3="Mix: "+str(round(mixes[0],2))+" dmg: "+str(round(self.mixtureDamage,2))
-			self.msg4="En: "+str(self.eng_type[0])+" F_m: "+str(round(m_fuel))+" F_a: "+str(round(a_fuel))
+			self.msg4="En: "+str(self.eng_type[0])" Prop: "+str(self.prop_type[0])
 			self.createEventWindow()
 
 		return 1
