c5a37c35860cb03fe69c38f089b26353c3f6dfcd
Nattgew <Nattgew@users.noreply.github.com>
2014-08-30 17:38:23 -0700

    Fix up showing of change in setting

--- PI_altimeterhelper.py
+++ PI_altimeterhelper.py
@@ -18,13 +18,14 @@ class PythonInterface:
 		bar_old=XPLMGetDataf(self.baro_set_ref)
 		XPLMSetDataf(self.baro_set_ref, bar_new)
 		del_baro_set=bar_new-bar_old
-		alt_new=XPLMGetDataf(self.alt_ind_ref)
+		alt_new=1000.0000238427*bar_new-29480.049077438
 		del_alt_ind=alt_new-alt_old
 		
 		del_baro_str=self.getSign(del_baro_set)+str(round(del_baro_set,2))
-		del_alt_str=self.getSign(del_alt_ind)+str(round(del_alt_ind))
+		del_alt_str=self.getSign(del_alt_ind)+str(int(round(del_alt_ind)))
 		
 		print "Altimeter changed to: " + str(round(bar_new,2))
+		#print "From "+str(int(round(alt_old)))+" to "+str(int(round(alt_new)))
 		self.msg1="Altimeter  " + str(round(bar_new,2))
 		self.msg2=del_alt_str+" ft   "+del_baro_str+" inHg"
 		self.remainingShowTime=self.showTime
@@ -51,19 +52,20 @@ class PythonInterface:
 		self.showTime=3
 		winPosX=20
 		winPosY=500
-		win_w=150
+		win_w=170
 		win_h=50
-		self.last_alt=0
+		self.last_alt=XPLMGetDataf(self.alt_ind_ref)
 		self.trans_alt=18000
-		self.last_baro_set=29.92
+		self.last_bar=XPLMGetDataf(self.baro_set_ref)
 
 		self.DrawWindowCB=self.DrawWindowCallback
 		self.KeyCB=self.KeyCallback
 		self.MouseClickCB=self.MouseClickCallback
 		self.gWindow=XPLMCreateWindow(self, winPosX, winPosY, winPosX + win_w, winPosY - win_h, 1, self.DrawWindowCB, self.KeyCB, self.MouseClickCB, 0)
 		
+		#print "Registering loopback"
 		self.gameLoopCB=self.gameLoopCallback
-		XPLMRegisterFlightLoopCallback(self, self.gameLoopCB, 0.05, 0)
+		XPLMRegisterFlightLoopCallback(self, self.gameLoopCB, 1, 0)
 		
 		self.CmdSHConn = XPLMCreateCommand("althelp/tools/set_altimeter","Sets altimeter for current position and altitude")
 		self.CmdSHConnCB  = self.CmdSHConnCallback
@@ -84,6 +86,7 @@ class PythonInterface:
 				newbaro=29.92
 			else:
 				newbaro=XPLMGetDataf(self.baro_act_ref)
+			self.last_bar=newbaro
 			alt=self.setBaro(newbaro)
 		return 0
 
@@ -120,6 +123,7 @@ class PythonInterface:
 		alt=XPLMGetDataf(self.alt_ind_ref)
 		vvi=XPLMGetDataf(self.vvi_ref)
 		alt_act=XPLMGetDataf(self.alt_act_ref)*self.mft
+		bar=XPLMGetDataf(self.baro_set_ref)
 		
 		if alt >= self.trans_alt and self.last_alt < self.trans_alt:
 			# Climbing through 18000
@@ -138,8 +142,14 @@ class PythonInterface:
 			self.msg1="Update altimeter setting!"
 			self.msg2="Indicated off by "+alt_err_str+" feet!"
 			self.remainingShowTime=self.showTime
+			
+		if round(self.last_bar,2)!=round(bar,2):
+			#print "Setting changed from "+str(round(self.last_bar,2))+" to "+str(round(bar,2))
+			alt=self.setBaro(bar)
 
 		self.last_alt=alt
+		self.last_bar=bar
+		#print str(alt)+"  "+str(bar)
 
-		return 0.05
+		return 0.1
 		
