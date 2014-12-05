913ba28ab0541109c7c8b7be97ef67a2f8323996
Nattgew <Nattgew@users.noreply.github.com>
2014-12-05 10:57:44 -0800

    Cleanup, add comments, test the error calculation

--- PI_altimeterhelper.py
+++ PI_altimeterhelper.py
@@ -7,33 +7,28 @@ from XPLMDefs import *
 
 class PythonInterface:
 
-	def getSign(self, val):
+	def getSign(self, val): #Puts a plus sign in front of positive values
 		sign=""
 		if val>0:
 			sign="+"
 		return sign
 	
-	def getAlt(self, SL, AM):
+	def getAlt(self, SL, AM): #Determines altitude based on pressure delta
 		alt=1000*(SL-AM)
 		return alt
 	
-	def setBaro(self, bar_new):
+	def setBaro(self, bar_new): #Set the barometer
 		bar_old=XPLMGetDataf(self.baro_set_ref)
-		#bar_am=XPLMGetDataf(self.baro_am_ref)
 		XPLMSetDataf(self.baro_set_ref, bar_new)
 		del_baro_set=bar_new-bar_old
-		#alt_new=self.getAlt(bar_new,bar_am)
-		
 		del_baro_str=self.getSign(del_baro_set)+str(round(del_baro_set,2))
-		
 		print "Altimeter changed to: " + str(round(bar_new,2))
-		#print "From "+str(int(round(alt_old)))+" to "+str(int(round(alt_new)))
 		self.msg1="Altimeter  " + str(round(bar_new,2))+"  "+del_baro_str+" inHg"
 		self.remainingShowTime=self.showTime
 		
 		pass
 	
-	def showBaro(self, bar_new):
+	def showBaro(self, bar_new): #Show the barometer setting
 		self.msg1="Altimeter  " + str(round(bar_new,2))
 		self.remainingShowTime=self.showTime
 		pass
@@ -51,17 +46,17 @@ class PythonInterface:
 		self.alt_ind_ref=XPLMFindDataRef("sim/flightmodel/misc/h_ind")
 		self.vvi_ref=XPLMFindDataRef("sim/flightmodel/position/vh_ind_fpm")
 		
-		self.mft=3.2808399
+		self.mft=3.2808399 #m -> ft
 		self.msg1=""
 		self.remainingShowTime=0
-		self.showTime=3
+		self.showTime=3 #Seconds to show the altimeter setting when changed
 		winPosX=20
 		winPosY=500
 		win_w=200
 		win_h=35
-		self.stdpress=0
+		self.stdpress=0 #Whether standard pressure is set
 		self.trans_alt=18000
-		self.tol=[17.009, 0.0058579, -0.000000012525]
+		self.tol=[17.009, 0.0058579, -0.000000012525] #Parameters for altimeter tolerance
 		self.last_bar=XPLMGetDataf(self.baro_set_ref)
 
 		self.DrawWindowCB=self.DrawWindowCallback
@@ -123,45 +118,36 @@ class PythonInterface:
 		pass
 
 	def gameLoopCallback(self, inElapsedSinceLastCall, elapsedSim, counter, refcon):
-		
 		if 0.0 < self.remainingShowTime:
 			self.remainingShowTime -= inElapsedSinceLastCall
 		
-		alt=XPLMGetDataf(self.alt_ind_ref)
-		vvi=XPLMGetDataf(self.vvi_ref)
-		alt_act=XPLMGetDataf(self.alt_act_ref)*self.mft
-		bar=XPLMGetDataf(self.baro_set_ref)
-		bar_am=XPLMGetDataf(self.baro_am_ref)
-		bar_act=XPLMGetDataf(self.baro_act_ref)
+		alt=XPLMGetDataf(self.alt_ind_ref) #Indicated altitude, ft
+		vvi=XPLMGetDataf(self.vvi_ref) #Vertical speed, fpm
+		alt_act=XPLMGetDataf(self.alt_act_ref)*self.mft #Real altitude, convert to ft
+		bar=XPLMGetDataf(self.baro_set_ref) #Current altimeter setting
+		bar_am=XPLMGetDataf(self.baro_am_ref) #Current outside barometric pressure
+		bar_act=XPLMGetDataf(self.baro_act_ref) #Current local sea level pressure
 		
-		if (vvi >= 500 and alt >= (self.trans_alt-25) or vvi < 500 and vvi > 0 and alt > self.trans_alt + 250) and self.stdpress==0:
-			# Climbing through 18000
-			print "Climbing through transition alt"
+		if (vvi >= 500 and alt >= (self.trans_alt-25) or vvi < 500 and vvi > 0 and alt > self.trans_alt + 250) and self.stdpress==0: # Climbing through 18000
 			bar=29.92
 			self.stdpress=1
 			self.setBaro(bar)
-		elif (vvi <= -500 and alt < (self.trans_alt-25) or vvi > -500 and vvi < 0 and alt < self.trans_alt - 250) and self.stdpress==1:
-			# Descending through 18000
-			print "Descending through transition alt"
+		elif (vvi <= -500 and alt < (self.trans_alt-25) or vvi > -500 and vvi < 0 and alt < self.trans_alt - 250) and self.stdpress==1: # Descending through 18000
 			bar=XPLMGetDataf(self.baro_act_ref)
 			self.stdpress=0
 			self.setBaro(bar)
 		
 		alt_err=self.getAlt(bar-bar_act,0)
-		tolerance=self.tol[2]*alt**2+self.tol[1]*alt+self.tol[0]
+		alt_err=(alt-alt_act)
+		tolerance=self.tol[2]*alt**2+self.tol[1]*alt+self.tol[0] #Determine altimeter error tolerance
 		if abs(alt_err)>tolerance and self.stdpress==0:
 			alt_err_str=self.getSign(alt_err)+str(round(alt_err))
 			self.msg1="Altimeter off by "+alt_err_str+" feet!"
 			self.remainingShowTime=self.showTime
 		
 		if round(self.last_bar,2)!=round(bar,2):
-			#print "Setting changed from "+str(round(self.last_bar,2))+" to "+str(round(bar,2))
 			self.showBaro(bar)
 
 		self.last_bar=bar
 		
-		#ind=self.getAlt(bar,bar_am)
-		#diff=ind-alt
-		#print str(ind)+" vs "+str(alt)+" diff "+str(diff)
-
 		return 0.1
