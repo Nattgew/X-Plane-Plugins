5c79c0eef73f28c9d36cf09ff0adae5226f442e0
Nattgew <Nattgew@users.noreply.github.com>
2014-12-05 11:46:47 -0800

    Change per-aircraft stuff

--- PI_altimeterhelper.py
+++ PI_altimeterhelper.py
@@ -2,109 +2,129 @@ from XPLMProcessing import *
 from XPLMDataAccess import *
 from XPLMDisplay import *
 from XPLMGraphics import *
-from XPLMUtilities import *
 from XPLMDefs import *
+from XPLMUtilities import *
 
 class PythonInterface:
 
-	def getSign(self, val): #Puts a plus sign in front of positive values
-		sign=""
-		if val>0:
-			sign="+"
-		return sign
-	
-	def getAlt(self, SL, AM): #Determines altitude based on pressure delta
-		alt=1000*(SL-AM)
-		return alt
-	
-	def setBaro(self, bar_new): #Set the barometer
-		bar_old=XPLMGetDataf(self.baro_set_ref)
-		XPLMSetDataf(self.baro_set_ref, bar_new)
-		del_baro_set=bar_new-bar_old
-		del_baro_str=self.getSign(del_baro_set)+str(round(del_baro_set,2))
-		print "Altimeter changed to: " + str(round(bar_new,2))
-		self.msg1="Altimeter  " + str(round(bar_new,2))+"  "+del_baro_str+" inHg"
-		self.remainingShowTime=self.showTime
-		
-		pass
-	
-	def showBaro(self, bar_new): #Show the barometer setting
-		self.msg1="Altimeter  " + str(round(bar_new,2))
-		self.remainingShowTime=self.showTime
-		pass
-
 	def XPluginStart(self):
-		self.Name="Altimeter Helper 1.2"
-		self.Sig= "natt.python.altimeterhelper"
-		self.Desc="A plugin that helps with altimeter settings"
-		self.VERSION="1.2"
+		self.Name="PCAT"
+		self.Sig= "natt.python.pcat"
+		self.Desc="Basic auto-throttle"
+		self.VERSION="0.1"
 		
-		self.baro_set_ref=XPLMFindDataRef("sim/cockpit/misc/barometer_setting")
-		self.baro_act_ref=XPLMFindDataRef("sim/weather/barometer_sealevel_inhg")
-		self.baro_am_ref=XPLMFindDataRef("sim/weather/barometer_current_inhg")
-		self.alt_act_ref=XPLMFindDataRef("sim/flightmodel/position/elevation")
-		self.alt_ind_ref=XPLMFindDataRef("sim/flightmodel/misc/h_ind")
-		self.vvi_ref=XPLMFindDataRef("sim/flightmodel/position/vh_ind_fpm")
-		
-		self.mft=3.2808399 #m -> ft
-		self.msg1=""
-		self.remainingShowTime=0
-		self.showTime=3 #Seconds to show the altimeter setting when changed
-		winPosX=20
-		winPosY=500
-		win_w=200
-		win_h=35
-		self.stdpress=0 #Whether standard pressure is set
-		self.trans_alt=18000
-		self.tol=[17.009, 0.0058579, -0.000000012525] #Parameters for altimeter tolerance
-		self.last_bar=XPLMGetDataf(self.baro_set_ref)
+		self.TO_ref=XPLMFindDataRef("sim/operation/override/override_throttles") #	int	710+	yes	boolean	Override the throttles (use ENGN_thro_use to control them)
+		self.thrott_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_thro") #	float[8]	660+	yes	ratio	Throttle (per engine) as set by user, 0 = idle, 1 = max
+		self.throtto_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_thro_use") #	float[8]	710+	yes	ratio 	Throttle (per engine) when overridden by you, plus with thrust vectors - use override_throttles to change.
+		self.TRQ_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_TRQ") #	float[8]	660+	yes	NewtonMeters	Torque (per engine)
+		self.N1_ref=XPLMFindDataRef("sim/flightmodel/engine/ENGN_N1_")
+		self.num_eng_ref=XPLMFindDataRef("sim/aircraft/engine/acf_num_engines")
+		self.ias_ref=XPLMFindDataRef("sim/flightmodel/position/indicated_airspeed")
+		self.alpha_ref=XPLMFindDataRef("sim/flightmodel/position/alpha")
+		self.alphawarn_ref=XPLMFindDataRef("sim/aircraft/overflow/acf_stall_warn_alpha")
+		self.stallwarn_ref=XPLMFindDataRef("sim/cockpit2/annunciators/stall_warning")
+		self.acf_desc_ref=XPLMFindDataRef("sim/aircraft/view/acf_descrip")
+		self.ap_as_ref=XPLMFindDataRef("sim/cockpit/autopilot/airspeed")
+		self.ap_asmach_ref=XPLMFindDataRef("sim/cockpit/autopilot/airspeed_is_mach")
+		self.eng_type_ref=XPLMFindDataRef("sim/aircraft/prop/acf_en_type")
 
+		self.Nlb=0.22481 # N to lbf
+		self.mft=3.2808399 # m to ft
+		self.Kp=1.0 # PID controller gains
+		self.Ki=0.005
+		self.Kd=10.0
+		self.Integrator_max=10
+		self.Integrator_min=-10
+		self.Integrator=0
+		self.Derivator=0
+		self.IAS=140.0
+		self.started=0
+		winPosX=20
+		winPosY=400
+		win_w=230
+		win_h=80
+		self.msg=[]
+		for i in range(0,5):
+			self.msg.append("")
+		self.ac=""
+		self.eng_type=[]
+		
+		self.gameLoopCB=self.gameLoopCallback
 		self.DrawWindowCB=self.DrawWindowCallback
 		self.KeyCB=self.KeyCallback
 		self.MouseClickCB=self.MouseClickCallback
 		self.gWindow=XPLMCreateWindow(self, winPosX, winPosY, winPosX + win_w, winPosY - win_h, 1, self.DrawWindowCB, self.KeyCB, self.MouseClickCB, 0)
-		
-		self.gameLoopCB=self.gameLoopCallback
-		XPLMRegisterFlightLoopCallback(self, self.gameLoopCB, 1, 0)
-		
-		self.CmdSHConn = XPLMCreateCommand("althelp/set_altimeter","Sets altimeter for current position and altitude")
-		self.CmdSHConnCB  = self.CmdSHConnCallback
-		XPLMRegisterCommandHandler(self, self.CmdSHConn,  self.CmdSHConnCB, 0, 0)
-		
-		return self.Name, self.Sig, self.Desc
+		self.CmdATConn = XPLMCreateCommand("pcat/toggle","Starts or stops auto throttle")
+		self.CmdATConnCB = self.CmdATConnCallback
+		XPLMRegisterCommandHandler(self, self.CmdATConn, self.CmdATConnCB, 0, 0)
+		self.CmdUpConn = XPLMCreateCommand("pcat/ias_up","Set IAS +1kt")
+		self.CmdUpConnCB = self.CmdUpConnCallback
+		XPLMRegisterCommandHandler(self, self.CmdUpConn, self.CmdUpConnCB, 0, 0)
+		self.CmdDnConn = XPLMCreateCommand("pcat/ias_down","Set IAS -1kt")
+		self.CmdDnConnCB = self.CmdDnConnCallback
+		XPLMRegisterCommandHandler(self, self.CmdDnConn, self.CmdDnConnCB, 0, 0)
 
+		return self.Name, self.Sig, self.Desc
+		
 	def MouseClickCallback(self, inWindowID, x, y, inMouse, inRefcon):
 		return 0
 
 	def KeyCallback(self, inWindowID, inKey, inFlags, inVirtualKey, inRefcon, losingFocus):
 		pass 
 
-	def CmdSHConnCallback(self, cmd, phase, refcon):
+	def CmdATConnCallback(self, cmd, phase, refcon):
 		if(phase==0): #KeyDown event
-			print "AltHelper = CMD set altimeter"
-			if XPLMGetDataf(self.alt_ind_ref) > self.trans_alt:
-				newbaro=29.92
-			else:
-				newbaro=XPLMGetDataf(self.baro_act_ref)
-			if round(newbaro,2)!=round(XPLMGetDataf(self.baro_set_ref),2):
-				self.last_bar=newbaro
-				self.setBaro(newbaro)
-			else:
-				self.showBaro(newbaro)
+			print "PCAT = Toggle AT"
+			self.toggleInfo()
 		return 0
-
-	def DrawWindowCallback(self, inWindowID, inRefcon):
-		if self.remainingShowTime > 0:
-			lLeft=[];	lTop=[];	lRight=[];	lBottom=[]
-			XPLMGetWindowGeometry(inWindowID, lLeft, lTop, lRight, lBottom)
-			left=int(lLeft[0]); top=int(lTop[0]); right=int(lRight[0]); bottom=int(lBottom[0])
-			XPLMDrawTranslucentDarkBox(left,top,right,bottom)
-			color=1.0, 1.0, 1.0
-			gResult=XPLMDrawString(color, left+5, top-20, self.msg1, 0, xplmFont_Basic)
-
+	
+	def CmdUpConnCallback(self, cmd, phase, refcon):
+		if(phase==0): #KeyDown event
+			print "PCAT = IAS +"
+			self.IAS+=1.0
+			XPLMSetDataf(self.ap_as_ref, self.IAS)
+		return 0
+	
+	def CmdDnConnCallback(self, cmd, phase, refcon):
+		if(phase==0): #KeyDown event
+			print "PCAT = IAS -"
+			self.IAS-=1.0
+			XPLMSetDataf(self.ap_as_ref, self.IAS)
+		return 0
+		
+	def toggleInfo(self):
+		if self.started==0:
+			XPLMSetDatai(self.ap_asmach_ref, 0)
+			acf_descb=[]
+			XPLMGetDatab(self.acf_desc_ref, acf_descb, 0, 500)
+			self.ac=self.getshortac(str(acf_descb))
+			self.num_eng=XPLMGetDatai(self.num_eng_ref)
+			XPLMSetDataf(self.ap_as_ref, self.IAS)
+			XPLMGetDatavi(self.eng_type_ref, self.eng_type, 0, self.num_eng)
+			#T=[]
+			#XPLMGetDatavf(self.thrott_ref, T, 0, self.num_eng)
+			#XPLMSetDatavf(self.throtto_ref, T, 0, self.num_eng)
+			#XPLMSetDatai(self.TO_ref, 1)
+			#override=XPLMGetDatai(self.TO_ref)
+			XPLMRegisterFlightLoopCallback(self, self.gameLoopCB, 0.5, 0)
+			self.started=1
+		else:
+			XPLMUnregisterFlightLoopCallback(self, self.gameLoopCB, 0)
+			self.ac=""
+			self.eng_type=[]
+			#T=[]
+			#XPLMGetDatavf(self.throtto_ref, T, 0, self.num_eng)
+			#XPLMSetDatavf(self.thrott_ref, T, 0, self.num_eng)
+			#XPLMSetDatai(self.TO_ref, 0)
+			self.started=0
+		pass
+		
 	def XPluginStop(self):
-		XPLMUnregisterCommandHandler(self, self.CmdSHConn, self.CmdSHConnCB, 0, 0)
-		XPLMUnregisterFlightLoopCallback(self, self.gameLoopCB, 0);
+		XPLMUnregisterCommandHandler(self, self.CmdATConn, self.CmdATConnCB, 0, 0)
+		XPLMUnregisterCommandHandler(self, self.CmdUpConn, self.CmdUpConnCB, 0, 0)
+		XPLMUnregisterCommandHandler(self, self.CmdDnConn, self.CmdDnConnCB, 0, 0)
+		if self.started==1:
+			self.toggleInfo()
 		XPLMDestroyWindow(self, self.gWindow)
 		pass
 
@@ -116,38 +136,106 @@ class PythonInterface:
 
 	def XPluginReceiveMessage(self, inFromWho, inMessage, inParam):
 		pass
+		
+	def DrawWindowCallback(self, inWindowID, inRefcon):
+		if self.started==1:
+			lLeft=[];	lTop=[]; lRight=[];	lBottom=[]
+			XPLMGetWindowGeometry(inWindowID, lLeft, lTop, lRight, lBottom)
+			left=int(lLeft[0]); top=int(lTop[0]); right=int(lRight[0]); bottom=int(lBottom[0])
+			XPLMDrawTranslucentDarkBox(left,top,right,bottom)
+			color=1.0, 1.0, 1.0
+			for i in range(0,5):
+				XPLMDrawString(color, left+5, top-(20+15*i), self.msg[i], 0, xplmFont_Basic)
 
 	def gameLoopCallback(self, inElapsedSinceLastCall, elapsedSim, counter, refcon):
-		if 0.0 < self.remainingShowTime:
-			self.remainingShowTime -= inElapsedSinceLastCall
+		kias=XPLMGetDataf(self.ias_ref)
+		TO=[]
+		XPLMGetDatavf(self.thrott_ref, TO, 0, self.num_eng)
+		alpha=XPLMGetDataf(self.alpha_ref)
+		awarn=XPLMGetDataf(self.alphawarn_ref)
+		swarn=XPLMGetDatai(self.stallwarn_ref)
+		err=self.IAS-kias
+		PID=self.update(err)
+		torque_psi=0.0
+		if alpha<awarn-0.25 and swarn==0:
+			if self.eng_type[0]==2 or self.eng_type[0]==8: #Turboprop
+				TRQ=[]
+				XPLMGetDatavf(self.TRQ_ref, TRQ, 0, self.num_eng)
+				if self.ac == "PC12":
+					#print "PCAT - setting PC12 throttle"
+					torque_psi=0.0088168441*TRQ[0]
+					if torque_psi > 36.95:
+						TO[0]-=0.02
+					else:
+						TO[0]+=PID/400.0
+				elif self.ac == "B190":
+					torque_ftlb1=self.Nlb*self.mft*TRQ[0]
+					torque_ftlb2=self.Nlb*self.mft*TRQ[1]
+					if torque_ftlb1>3750.0 or torque_ftlb2>3750.0:
+						TO[0]-=0.02
+						TO[1]-=0.02
+					else:
+						TO[0]+=PID/400.0
+						TO[1]+=PID/400.0
+				else:
+					for i in range(0,self.num_eng):
+						TO[i]+=PID/400.0	
+			elif self.eng_type[0]==4 or self.eng_type[0]==5: #Jet
+				N1=[]
+				XPLMGetDatavf(self.N1_ref, N1, 0, self.num_eng)
+				if self.ac == "CL30":
+					if N1[0]>95.0 or N1[1]>95.0:
+						TO[0]-=0.02
+						TO[1]-=0.02
+					else:
+						TO[0]+=PID/400.0
+						TO[1]+=PID/400.0
+				else:
+					for i in range(0,self.num_eng):
+						TO[i]+=PID/400.0
+		else:
+			#print "PCAT - STALL!"
+			for i in range(0,self.num_eng):
+				TO[i]=1.0
+		if TO[0]>1:
+			TO[0]=1.0
+		elif TO[0]<0:
+			TO[0]=0.0
 		
-		alt=XPLMGetDataf(self.alt_ind_ref) #Indicated altitude, ft
-		vvi=XPLMGetDataf(self.vvi_ref) #Vertical speed, fpm
-		alt_act=XPLMGetDataf(self.alt_act_ref)*self.mft #Real altitude, convert to ft
-		bar=XPLMGetDataf(self.baro_set_ref) #Current altimeter setting
-		bar_am=XPLMGetDataf(self.baro_am_ref) #Current outside barometric pressure
-		bar_act=XPLMGetDataf(self.baro_act_ref) #Current local sea level pressure
+		#override=XPLMGetDatai(self.TO_ref)
+		#print "PCAT - override "+str(override)
+		XPLMSetDatavf(self.thrott_ref, TO, 0, self.num_eng)
 		
-		if (vvi >= 500 and alt >= (self.trans_alt-25) or vvi < 500 and vvi > 0 and alt > self.trans_alt + 250) and self.stdpress==0: # Climbing through 18000
-			bar=29.92
-			self.stdpress=1
-			self.setBaro(bar)
-		elif (vvi <= -500 and alt < (self.trans_alt-25) or vvi > -500 and vvi < 0 and alt < self.trans_alt - 250) and self.stdpress==1: # Descending through 18000
-			bar=XPLMGetDataf(self.baro_act_ref)
-			self.stdpress=0
-			self.setBaro(bar)
+		self.msg[0]="err: "+str(round(err,2))+" a: "+str(round(alpha,1))+"/"+str(round(awarn,1))+" warn: "+str(swarn)
+		self.msg[1]="TO: "+str(round(TO[0],3))+" PID: "+str(round(PID,4))
+		self.msg[2]="ac: "+self.ac+" trq: "+str(round(torque_psi,2))+" psi "+str(round(TRQ[0],2))
 		
-		alt_err=self.getAlt(bar-bar_act,0)
-		alt_err=(alt-alt_act)
-		tolerance=self.tol[2]*alt**2+self.tol[1]*alt+self.tol[0] #Determine altimeter error tolerance
-		if abs(alt_err)>tolerance and self.stdpress==0:
-			alt_err_str=self.getSign(alt_err)+str(round(alt_err))
-			self.msg1="Altimeter off by "+alt_err_str+" feet!"
-			self.remainingShowTime=self.showTime
-		
-		if round(self.last_bar,2)!=round(bar,2):
-			self.showBaro(bar)
+		return 1
 
-		self.last_bar=bar
+	def update(self,error):
+		P_value = self.Kp * error
+		D_value = self.Kd * ( error - self.Derivator)
+		self.Derivator = error
+		self.Integrator += error
+		if self.Integrator > self.Integrator_max:
+			self.Integrator = self.Integrator_max
+		elif self.Integrator < self.Integrator_min:
+			self.Integrator = self.Integrator_min
+		I_value = self.Integrator * self.Ki
+
+		self.msg[3]=str(round(P_value,4))+" "+str(round(I_value,4))+" "+str(round(D_value,4))
+		PID = P_value + I_value + D_value
+
+		return PID
 		
-		return 0.1
+	def getshortac(self,acf_desc):
+		if acf_desc=="['Pilatus PC-12']":
+			ac_short="PC12"
+		elif acf_desc=="['wouldnt you like to be a pepper too']":
+			ac_short="B190"
+		elif acf_desc=="['ok']":
+			ac_short="CL30"
+			self.IAS=250.0
+		else:
+			ac_short=acf_desc
+		return ac_short
