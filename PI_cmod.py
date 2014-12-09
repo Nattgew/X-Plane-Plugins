b43834e96b9394721522e29c7dbde1916534604a
Nattgew <Nattgew@users.noreply.github.com>
2014-12-09 11:04:39 -0800

    New plugin for simple custom commands

---
+++ PI_cmod.py
@@ -0,0 +1,132 @@
+from XPLMDataAccess import *
+from XPLMDefs import *
+from XPLMUtilities import *
+
+class PythonInterface:
+
+	def XPluginStart(self):
+		self.Name="Controller Mods"
+		self.Sig= "natt.python.cmod"
+		self.Desc="Modifications for using controllers"
+		self.VERSION="0.1"
+		
+		self.acf_desc_ref=XPLMFindDataRef("sim/aircraft/view/acf_descrip")
+		self.speed_brake_ref=XPLMFindDataRef("sim/flightmodel/controls/sbrkrqst") #float -0.5=armed 0=off 1=max
+		self.landing_lights_ref=XPLMFindDataRef("sim/cockpit/electrical/landing_lights_on") #int
+		self.geardep_ref=XPLMFindDataRef("sim/aircraft/parts/acf_gear_deploy") #float array
+		self.gearhand_ref=XPLMFindDataRef("sim/cockpit/switches/gear_handle_status") #int
+		self.flap_h_pos_ref=XPLMFindDataRef("sim/cockpit2/controls/flap_ratio") #float handle position 0->1
+		self.ap_vvi_ref=XPLMFindDataRef("sim/cockpit/autopilot/vertical_velocity") #float fpm
+		
+		self.CmdSTConn = XPLMCreateCommand("cmod/toggle/speedbrake","Toggles speed brakes")
+		self.CmdSTConnCB = self.CmdSTConnCallback
+		XPLMRegisterCommandHandler(self, self.CmdSTConn, self.CmdSTConnCB, 0, 0)
+		
+		self.CmdLTConn = XPLMCreateCommand("cmod/toggle/landinggearlights","Toggles speed brakes")
+		self.CmdLTConnCB = self.CmdLTConnCallback
+		XPLMRegisterCommandHandler(self, self.CmdLTConn, self.CmdLTConnCB, 0, 0)
+		
+		self.CmdFTConn = XPLMCreateCommand("cmod/toggle/flaps","Toggles through flaps settings")
+		self.CmdFTConnCB = self.CmdFTConnCallback
+		XPLMRegisterCommandHandler(self, self.CmdFTConn, self.CmdFTConnCB, 0, 0)
+		
+		self.CmdVSupConn = XPLMCreateCommand("cmod/custom/vspeed_up","AP vertical speed set +100 fpm")
+		self.CmdVSupConnCB = self.CmdVSupConnCallback
+		XPLMRegisterCommandHandler(self, self.CmdVSupConn, self.CmdVSupConnCB, 0, 0)
+		
+		self.CmdVSdnConn = XPLMCreateCommand("cmod/custom/vspeed_down","AP vertical speed set -100 fpm")
+		self.CmdVSdnConnCB = self.CmdVSdnConnCallback
+		XPLMRegisterCommandHandler(self, self.CmdVSdnConn, self.CmdVSdnConnCB, 0, 0)
+
+		return self.Name, self.Sig, self.Desc
+		
+	def CmdSTConnCallback(self, cmd, phase, refcon): #Toggle speedbrakes
+		if(phase==0): #KeyDown event
+			position=XPLMGetDataf(self.speed_brake_ref)
+			if position<1: #armed, stowed, or not fully deployed
+				XPLMSetDataf(self.speed_brake_ref, 1.0)
+			else: #fully deployed
+				XPLMSetDataf(self.speed_brake_ref, 0.0)
+		return 0
+	
+	def CmdLTConnCallback(self, cmd, phase, refcon): #Toggle landing gear and lights together
+		if(phase==0): #KeyDown event
+			gear=XPLMGetDatai(self.gearhand_ref)
+			light=XPLMGetDatai(self.landing_lights_ref)
+			if gear==0 or light==0: #Gear handle up->down, may need second go at landing light
+				XPLMSetDatai(self.gearhand_ref, 1)
+				XPLMSetDatai(self.landing_lights_ref, 1)
+			else: #Gear handle down->up
+				XPLMSetDatai(self.landing_lights_ref, 0)
+				XPLMSetDatai(self.gearhand_ref, 0)
+		return 0
+	
+	def CmdFTConnCallback(self, cmd, phase, refcon): #Flaps +1, retract if fully deployed
+		if(phase==0): #KeyDown event
+			acf_descb=[]
+			XPLMGetDatab(self.acf_desc_ref, acf_descb, 0, 500)
+			self.ac=self.getshortac(str(acf_descb))
+			handle=XPLMGetDataf(self.flap_h_pos_ref)
+			if handle==1: #Fully deployed
+				XPLMSetDataf(self.flap_h_pos_ref, 0.0) #Retract full
+			else: #Set next detent
+				if ac=="PC12":
+					flaps=(0.3,0.7,1.0) #15 30 40
+					self.nextflaps(handle, flaps)
+				elif ac=="B738":
+					flaps=(0.125,0.375,0.625,0.875,1.0) #1 5 15 30 40
+					self.nextflaps(handle, flaps)
+				else: #Fully deploy
+					XPLMSetDataf(self.flap_h_pos_ref, 1.0)
+		return 0
+	
+	def CmdVSupConnCallback(self, cmd, phase, refcon): #AP vertical speed +100 fpm
+		if(phase==0): #KeyDown event
+			vvi=XPLMGetDataf(self.ap_vvi_ref)
+			XPLMSetDataf(self.ap_vvi_ref, vvi+100)
+		return 0
+	
+	def CmdVSdnConnCallback(self, cmd, phase, refcon): #AP vertical speed -100 fpm
+		if(phase==0): #KeyDown event
+			vvi=XPLMGetDataf(self.ap_vvi_ref)
+			XPLMSetDataf(self.ap_vvi_ref, vvi-100)
+		return 0
+	
+	def nextflaps(self,handle,flaps):
+		i=0
+		while handle<flaps[i]+0.001:
+			i+=1
+		XPLMSetDataf(self.flap_h_pos_ref, flaps[i])
+	
+	def getshortac(self,acf_desc):
+		if acf_desc[0:27]=="['Boeing 737-800 xversion 4":
+			AC="B738"
+		elif acf_desc=="['Pilatus PC-12']":
+			AC="PC12"
+		elif acf_desc[0:9]=="['BE1900D":
+			AC="B190"
+		elif acf_desc=="['Bombardier Challenger 300']":
+			AC="CL30"
+		elif acf_desc[0:21]=="['C208B Grand Caravan":
+			AC="C208"
+		else:
+			AC=acf_desc
+			print str(acf_desc)
+		return ac_short
+	
+	def XPluginStop(self):
+		XPLMUnregisterCommandHandler(self, self.CmdSTConn, self.CmdSTConnCB, 0, 0)
+		XPLMUnregisterCommandHandler(self, self.CmdLTConn, self.CmdLTConnCB, 0, 0)
+		XPLMUnregisterCommandHandler(self, self.CmdFTConn, self.CmdFTConnCB, 0, 0)
+		XPLMUnregisterCommandHandler(self, self.CmdVSupConn, self.CmdVSupConnCB, 0, 0)
+		XPLMUnregisterCommandHandler(self, self.CmdVSdnConn, self.CmdVSdnConnCB, 0, 0)
+		pass
+
+	def XPluginEnable(self):
+		return 1
+
+	def XPluginDisable(self):
+		pass
+
+	def XPluginReceiveMessage(self, inFromWho, inMessage, inParam):
+		pass
