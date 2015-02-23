949de83f371d235adbb596928319c27d4b24a48a
Nattgew <Nattgew@users.noreply.github.com>
2015-02-23 12:54:03 -0800

    Update based on latest Python version

--- altimeterhelper.c
+++ altimeterhelper.c
@@ -1,9 +1,12 @@
 #include <string.h>
 #include <stdio.h>
-#include "XPLMDisplay.h"
-#include "XPLMGraphics.h"
-#include "XPLMDataAccess.h"
-#include "XPLMProcessing.h"
+#include <stdlib.h>
+#include <math.h>
+#include "SDK/CHeaders/XPLM/XPLMUtilities.h"
+#include "SDK/CHeaders/XPLM/XPLMDisplay.h"
+#include "SDK/CHeaders/XPLM/XPLMGraphics.h"
+#include "SDK/CHeaders/XPLM/XPLMDataAccess.h"
+#include "SDK/CHeaders/XPLM/XPLMProcessing.h"
 
 #include <GL/glew.h>
 
@@ -13,69 +16,108 @@ static float gameLoopCallback(float inElapsedSinceLastCall,
 
 static void	MyHotKeyCallback(void *	inRefcon);
 
-#define WINDOW_WIDTH 130
-#define WINDOW_HEIGHT 80
-#define FEET 3.28084
+#define WINDOW_WIDTH 200
+#define WINDOW_HEIGHT 35
+#define FEET 3.28084 //m -> ft
 
 static XPLMHotKeyID	gHotKey = NULL;
 static XPLMWindowID gWindow = NULL;
-static char ChgMsg[17];
-static XPLMDataRef baro_set_ref, baro_act_ref, flightTimeRef, alt_act_ref, alt_ind_ref, vvi_ref;
-static float last_baro_set, last_alt;
+static char ChgMsg[64];
+static XPLMDataRef baro_set_ref, baro_act_ref, baro_am_ref, alt_act_ref, alt_ind_ref, vvi_ref;
+static XPLMCommandRef CmdSHConn;
+static float last_bar, last_alt;
 static float remainingShowTime = 0.0f;
 //static float remainingUpdateTime = 0.0f;
 
-static int showTime = 2.0f;
+static int showTime = 2.0f; //Seconds to show the altimeter setting when changed
 static int winPosX = 20;
-static int winPosY = 600;
+static int winPosY = 500;
 static int lastMouseX, lastMouseY;
 static int windowCloseRequest = 0;
+static int stdpress=0; //Whether standard pressure is set
+static int trans_alt=18000;
+static float tol[3];
+tol = [17.009, 0.0058579, -0.000000012525]; //Parameters for altimeter tolerance
 
+char getSign(float val);
+float getAlt(float SL, float AM);
+void setBaro(float bar_new);
+void showBaro(float bar_new);
+int CmdSHConnCB(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void * inRefcon);
+
+char getSign(float val) { //Puts a plus sign in front of positive values
+	char sign;
+	if (val>0)
+		sign='+';
+	return sign;
+}
+
+float getAlt(float SL, float AM) { //Determines altitude based on pressure delta
+	float alt;
+	alt=1000*(SL-AM);
+	return alt;
+}
+
+void setBaro(float bar_new) { //Set the barometer
+	float bar_old, del_baro_set;
+	char *del_baro_str, *buf;
+	size_t sz;
+	bar_old=XPLMGetDataf(baro_set_ref);
+	XPLMSetDataf(baro_set_ref, bar_new);
+	del_baro_set=bar_new-bar_old;
+	sz = snprintf(NULL,0,"Altimeter changed to: %.2f",bar_new);
+	buf = (char *)malloc(sz+1);
+	sz = snprintf(buf,sz+1,"Altimeter changed to: %.2f",bar_new);
+	XPLMDebugString(buf);
+	free(buf);
+	sz = snprintf(NULL,0,"Altimeter  %.2f  %c%.2f inHg",bar_new,getSign(del_baro_set),del_baro_set);
+	buf = (char *)malloc(sz+1);
+	snprintf(buf,sz+1,"Altimeter  %.2f  %c%.2f inHg",bar_new,getSign(del_baro_set),del_baro_set);
+	strcpy(ChgMsg, buf);
+	remainingShowTime=showTime;
+}
+
+void showBaro(bar_new) { //Show the barometer setting
+	char *buf;
+	size_t sz;
+	sz=snprintf(NULL,0,"Altimeter  %.2f",bar_new);
+	buf=(char *)malloc(sz+1);
+	snprintf(buf,sz+1,"Altimeter  %.2f",bar_new);
+	strcpy(ChgMsg, buf);
+	remainingShowTime=showTime;
+}
 
 PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc)
 {
 
-	strcpy(outName, "Altimeter Helper 1.0");
+	strcpy(outName, "Altimeter Helper 1.2");
 	strcpy(outSig, "natt.altimeterhelper");
 	strcpy(outDesc, "A plugin that helps with altimeter settings.");
 
 	baro_set_ref = XPLMFindDataRef("sim/cockpit/misc/barometer_setting"); //   float	660+	yes	???	The pilots altimeter setting
 	baro_act_ref = XPLMFindDataRef("sim/weather/barometer_sealevel_inhg"); //   float	660+	yes	29.92 +- ....	The barometric pressure at sea level.
-	flightTimeRef = XPLMFindDataRef("sim/time/total_flight_time_sec"); //   float	660+	yes	seconds	Total time since the flight got reset by something
+	baro_am_ref = XPLMFindDataRef("sim/weather/barometer_current_inhg"); //   float	660+	no	29.92+-....	This is the barometric pressure at the point the current flight is at.
 	alt_act_ref = XPLMFindDataRef("sim/flightmodel/position/elevation"); // 	double	660+	no	meters	The elevation above MSL of the aircraft
 	alt_ind_ref = XPLMFindDataRef("sim/cockpit2/gauges/indicators/altitude_ft_pilot"); // 	float	900+	no	feet	Indicated height, MSL, in feet, primary system, based on pilots barometric pressure input.
 	vvi_ref = XPLMFindDataRef("sim/flightmodel/position/vh_ind_fpm"); // float 740+ yes fpm VVI (vertical velocity in feet per second)
 	
-	memset(ChgMsg, 0, sizeof(ChgMsg));
+	//memset(ChgMsg, 0, sizeof(ChgMsg));
 	XPLMRegisterFlightLoopCallback(gameLoopCallback, 0.05f, NULL);
-	gHotKey = XPLMRegisterHotKey(XPLM_VK_B, xplm_DownFlag+xplm_ShiftFlag+xplm_ControlFlag, 
-				"Sets altimeter automatically",
-				MyHotKeyCallback,
-				NULL);
+	gWindow=XPLMCreateWindow(winPosX, winPosY, winPosX + WINDOW_WIDTH, winPosY - WINDOW_HEIGHT, 1, drawWindowCallback, keyboardCallback, mouseCallback, 0);
 
+	CmdSHConn = XPLMCreateCommand("althelp/set_altimeter","Sets altimeter for current position and altitude");
+	XPLMRegisterCommandHandler(CmdSHConn, CmdSHConnCB, 0, (void *) 0);
+	
 	//printf("ALTI - plugin loaded\n");	
 
 	return 1;
 }
 
-
-static void closeEventWindow()
-{
-	
-	if (gWindow) {
-		XPLMDestroyWindow(gWindow);
-		gWindow = NULL;
-	}
-	
-	memset(ChgMsg, 0, sizeof(ChgMsg));
-	remainingShowTime = 0.0f;
-}
-
 PLUGIN_API void	XPluginStop(void)
 {
-	XPLMUnregisterHotKey(gHotKey);
+	XPLMUnregisterCommandHandler(CmdSHConn, CmdSHConnCB, 0, 0);
 	XPLMUnregisterFlightLoopCallback(gameLoopCallback, NULL);
-	closeEventWindow();
+	XPLMDestroyWindow(gWindow);
 }
 
 PLUGIN_API void XPluginDisable(void)
@@ -92,24 +134,32 @@ PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFromWho,
 {
 }
 
-void	MyHotKeyCallback(void *			   inRefcon)
-{
-	float alt, newbaro, baroact;
-	alt = XPLMGetDataf(alt_ind_ref);
-	baroact = XPLMGetDataf(baro_act_ref);
-	if (alt > 18000.0) {
-		newbaro = 29.92;
-	} else {
-		newbaro = baroact;
+int CmdSHConnCB(XPLMCommandRef cmd, XPLMCommandPhase phase, void * refcon) {
+	if (phase==0) { //KeyDown event
+		float newbaro;
+		XPLMDebugString("AltHelper = CMD set altimeter\n");
+		if (XPLMGetDataf(alt_ind_ref) > trans_alt) {
+			newbaro=29.92;
+		} else {
+			newbaro=XPLMGetDataf(baro_act_ref);
+		}
+		newbaro_hund=newbaro*100;
+		baroset_hund=XPLMGetDataf(baro_set_ref)*100;
+		newbaro_hund >= 0 ? (long)(newbaro_hund+0.5) : (long)(newbaro_hund-0.5);
+		baroset_hund >= 0 ? (long)(baroset_hund+0.5) : (long)(baroset_hund-0.5);
+		if (abs(newbaro_hund-baroset_hund)>0) {
+			last_bar=newbaro;
+			setBaro(newbaro);
+		} else {
+			showBaro(newbaro);
+		}
 	}
-		
-	XPLMSetDataf(baro_set_ref, newbaro);
-	//printf("ALTI - Alt ind: %.0f - Set altimeter to: %.2f\n",alt,newbaro);
+	return 0;
 }
 
 void drawWindowCallback(XPLMWindowID inWindowID, void *inRefcon)
 {
-	//printf("ALTI - DrawWindow\n");
+	//XPLMDebugString("ALTI - DrawWindow\n");
 
 	if (0.0f < remainingShowTime) {
 		int left, top, right, bottom;
@@ -135,34 +185,7 @@ void drawWindowCallback(XPLMWindowID inWindowID, void *inRefcon)
 static int mouseCallback(XPLMWindowID inWindowID, int x, int y,	
 				   XPLMMouseStatus inMouse, void *inRefcon)
 {
-	if (windowCloseRequest)
-		return 1;
-	
-	switch (inMouse) {
-		case xplm_MouseDown:
-			if ((x >= winPosX + WINDOW_WIDTH - 8) && (x <= winPosX + WINDOW_WIDTH) && 
-						(y <= winPosY) && (y >= winPosY - 8))
-				windowCloseRequest = 1;
-			else {
-				lastMouseX = x;
-				lastMouseY = y;
-			}
-			break;
-			
-		case xplm_MouseDrag:
-			winPosX += x - lastMouseX;
-			winPosY += y - lastMouseY;
-			XPLMSetWindowGeometry(gWindow, winPosX, winPosY, 
-					winPosX + WINDOW_WIDTH, winPosY - WINDOW_HEIGHT);
-			lastMouseX = x;
-			lastMouseY = y;
-			break;
-			
-		case xplm_MouseUp:
-			break;
-	}
-
-	return 1;
+	return 0;
 }
 
 static void keyboardCallback(XPLMWindowID inWindowID, char inKey, XPLMKeyFlags inFlags,	
@@ -170,74 +193,49 @@ static void keyboardCallback(XPLMWindowID inWindowID, char inKey, XPLMKeyFlags i
 {
 }
 
-
-static void createEventWindow()
-{
-	remainingShowTime = showTime;
-	//remainingUpdateTime = 1.0f;
-	if (! gWindow)
-		gWindow = XPLMCreateWindow(winPosX, winPosY, 
-					winPosX + WINDOW_WIDTH, winPosY - WINDOW_HEIGHT, 
-					1, drawWindowCallback, keyboardCallback, 
-					mouseCallback, NULL);
-}
-
-
 static float gameLoopCallback(float inElapsedSinceLastCall,
 				float inElapsedTimeSinceLastFlightLoop, int inCounter,	
 				void *inRefcon)
 {
 	float alt = XPLMGetDataf(alt_ind_ref), vvi = XPLMGetDataf(vvi_ref);
-	if (alt >= 18000.0 && last_alt < 18000.0) {
-		// Climbing through 18000
-		if (vvi >= 500.0 || (vvi < 500.0 && alt > 18250.0)) {
-			XPLMSetDataf(baro_set_ref, 29.92);
-			alt = XPLMGetDataf(alt_ind_ref);
-		}
-	} else if (alt < 18000.0 && last_alt >= 18000.0) {
-		// Descending through 18000
-		if (vvi < -500.0 || (vvi > -500.0 && alt < 17750.0)) {
-			XPLMSetDataf(baro_set_ref, XPLMGetDataf(baro_act_ref));
-			alt = XPLMGetDataf(alt_ind_ref);
-		}
-	}	
+	float alt_act=XPLMGetDataf(alt_act_ref)*FEET, bar=XPLMGetDataf(baro_set_ref);
+	float bar_am=XPLMGetDataf(baro_am_ref), bar_act=XPLMGetDataf(baro_act_ref);
+	float alt_err, tolerance;
 	
-	float timeFromStart = XPLMGetDataf(flightTimeRef);
-	float baro_set = XPLMGetDataf(baro_set_ref);
-
-	//printf("ALTI - flight loop, tfs %.2f\n",timeFromStart);
-	//printf("ALTI - P: %.2f   C: %.2f  remain: %.0f  tfs:%.2f\n",last_baro_set,baro_set,remainingUpdateTime,timeFromStart);
-
-	if (3.0 < timeFromStart) {
-	//printf("ALTI - over 3 from start\n");
-		if (windowCloseRequest) {
-			windowCloseRequest = 0;
-		//printf("ALTI - Close window\n");
-			closeEventWindow();
-		} /*else
-			if (0.0 < remainingUpdateTime) {
-				remainingUpdateTime -= inElapsedSinceLastCall;
-				if (0.0 > remainingUpdateTime)
-					remainingUpdateTime = 0.0;
-			printf("ALTI - 0 remain time\n");
-			}*/
-		if (0.0f < remainingShowTime) {
-			remainingShowTime -= inElapsedSinceLastCall;
-			//printf("ALTI - cdown show time\n");
-			if (0.0f >= remainingShowTime)
-				closeEventWindow();
-		//printf("ALTI - close window\n");
-		}
-		if (baro_set != last_baro_set) {
-			//printf("ALTI - altimeter changed to: %.2f\n",baro_set);
-			sprintf(ChgMsg, "Altimeter  %.2f", baro_set);
-			//printf("ALTI - create window\n");
-			createEventWindow();
-		}
+	if ( 0 < remainingShowTime)
+		remainingShowTime -= inElapsedSinceLastCall;
+	
+	if ((vvi >= 500 && alt >= (trans_alt-25) || vvi < 500 && vvi > 0 && alt > trans_alt+250) && stdpress==0) { // Climbing through 18000
+		bar=29.92;
+		stdpress=1;
+		setBaro(bar);
+	} else if ((vvi <= -500 && alt < (trans_alt-25) || vvi > -500 && vvi < 0 && alt < trans_alt - 250) && stdpress==1) { // Descending through 18000
+		bar=XPLMGetDataf(baro_act_ref);
+		stdpress=0;
+		setBaro(bar);
 	}
 	
-	last_baro_set = baro_set;
+	alt_err=getAlt(bar-bar_act,0);
+	alt_err=(alt-alt_act);
+	tolerance=tol[2]*alt*alt+tol[1]*alt+tol[0]; //Determine altimeter error tolerance
+	if (fabs(alt_err)>tolerance and stdpress==0) {
+		char *buf;
+		size_t sz;
+		sz = snprintf(NULL,0,"Altimeter off by %c%.0f feet!",getSign(alt_err),alt_err);
+		buf = (char *)malloc(sz+1);
+		sz = snprintf(buf,sz+1,"Altimeter off by %c%.0f feet!",getSign(alt_err),alt_err);
+		strcpy(ChgMsg,buf);
+		remainingShowTime=showTime;
+	}
+	baro_hund=bar*100;
+	lastbar_hund=last_bar*100;
+	baro_hund >= 0 ? (long)(baro_hund+0.5) : (long)(baro_hund-0.5);
+	lastbar_hund >= 0 ? (long)(lastbar_hund+0.5) : (long)(lastbar_hund-0.5);
+	if (abs(baro_hund-lastbar_hund)>0) {
+		showBaro(bar);
+	
+	last_bar = bar;
 	last_alt = alt;
 
-	return 0.05f;
+	return 0.1f;
 }
