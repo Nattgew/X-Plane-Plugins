8c525f657ab11d0dca9d369c0334f059b3c03c94
Nattgew <Nattgew@users.noreply.github.com>
2015-10-09 15:31:45 -0700

    Totalling and plotting stuff started

--- pricelog.py
+++ pricelog.py
@@ -6,6 +6,7 @@ import os, re, fileinput, csv, sqlite3
 import locale, time
 import sys, getopt
 import dicts
+from datetime import timedelta, date
 from mpl_toolkits.basemap import Basemap
 from matplotlib.dates import DateFormatter
 import matplotlib.pyplot as plt
@@ -16,6 +17,14 @@ def getkey():
 		mykey = f.readline()
 	mykey=mykey.strip()
 	return mykey
+
+def getname():
+	file='/mnt/data/XPLANE10/XSDK/mykey.txt'
+	with open(file, 'r') as f:
+		nothing = f.readline()
+		myname = f.readline()
+	myname=myname.strip()
+	return myname
 
 def fserequest(rqst,tagname):
 	data = urllib.request.urlopen('http://server.fseconomy.net/data?userkey='+getkey()+'&format=xml&'+rqst)
@@ -209,7 +218,7 @@ def printjobs(jobs,rev):
 	else:
 		list=reversed(jobs)
 	for job in jobs:
-		print(job[2]+" "+job[3]+" "+job[0]+"-"+job[1]+" $"+str(int(job[4]))+" "+str(distbwt(job[0],job[1]))+" "+job[5])
+		print(job[2]+" "+job[3]+" "+job[0]+"-"+job[1]+" $"+str(int(job[4]))+" "+str(distbwt(job[0],job[1]])+" "+job[5])
 
 def cosinedist(lat1,lon1,lat2,lon2):
 	phi1 = math.radians(lat1)
@@ -271,7 +280,7 @@ def nearby(icao,rad):
 	clat,clon=loc_dict[icao]
 	for apt,coords in loc_dict.items():
 		if apt!=icao:
-			#print("Dist from "+str(clat)+" "+str(clon)+" to "+str(coords[0])+" "+str(coords[1]))
+			#print("Dist from "+str(clat)+" "+str(clon)+" to "+str(coords[0])+" "+str(coords[1]])
 			dist=cosinedist(clat,clon,coords[0],coords[1])
 			if dist<rad:
 				if near=="":
@@ -432,48 +441,417 @@ def plotdates(data,title,ylbl):
 
 def plotpayments(conn,fdate,tdate):
 	c=getpaydbcon(conn)
-	rentexp=0
-	rentinc=0
-	assnmtexp=0
-	assnmtinc=0
-	pltfees=0
-	addcrewfee=0
-	gndcrewfee=0
-	bkgfee=0
-	ref100=0
-	refjet=0
-	mxexp=0
-	eqinstl=0
-	acsold=0
-	acbought=0
-	fboref100=0
-	fborefjet=0
-	fbogndcrew=0
-	fborepinc=0
-	fborepexp=0
-	fboeqpexp=0
-	fboeqpinc=0
-	ptrentinc=0
-	ptrentexp=0
-	fbosell=0
-	fbobuy=0
-	wsbuy100=0
-	wssell100=0
-	wsbuyjet=0
-	wsselljet=0
-	wsbuybld=0
-	wssellbld=0
-	wsbuysupp=0
-	wssellsupp=0
-	grpay=0
+	rentexp, rentinc, assnmtexp, assnmtinc, pltfees, addcrewfee, gndcrewfee, bkgfee, ref100, refjet, mxexp, eqinstl, acsold, acbought, fboref100, fborefjet, fbogndcrew, fborepinc, fborepexp, fboeqpexp, fboeqpinc, ptrentinc, ptrentexp, fbosell, fbobuy, wsbuy100, wssell100, wsbuyjet, wsselljet, wsbuybld, wssellbld, wsbuysupp, wssellsupp, grpay=([fdate,0] for i in range(35))
+	allthat=[rentexp, rentinc, assnmtexp, assnmtinc, pltfees, addcrewfee, gndcrewfee, bkgfee, ref100, refjet, mxexp, eqinstl, acsold, acbought, fboref100, fborefjet, fbogndcrew, fborepinc, fborepexp, fboeqpexp, fboeqpinc, ptrentinc, ptrentexp, fbosell, fbobuy, wsbuy100, wssell100, wsbuyjet, wsselljet, wsbuybld, wssellbld, wsbuysupp, wssellsupp, grpay]
+	# In case my understanding of variables has a fatal flaw
+	# rentexp=[[fdate,0]]
+	# rentinc=[[fdate,0]]
+	# assnmtexp=[[fdate,0]]
+	# assnmtinc=[[fdate,0]]
+	# pltfees=[[fdate,0]]
+	# addcrewfee=[[fdate,0]]
+	# gndcrewfee=[[fdate,0]]
+	# bkgfee=[[fdate,0]]
+	# ref100=[[fdate,0]]
+	# refjet=[[fdate,0]]
+	# mxexp=[[fdate,0]]
+	# eqinstl=[[fdate,0]]
+	# acsold=[[fdate,0]]
+	# acbought=[[fdate,0]]
+	# fboref100=[[fdate,0]]
+	# fborefjet=[[fdate,0]]
+	# fbogndcrew=[[fdate,0]]
+	# fborepinc=[[fdate,0]]
+	# fborepexp=[[fdate,0]]
+	# fboeqpexp=[[fdate,0]]
+	# fboeqpinc=[[fdate,0]]
+	# ptrentinc=[[fdate,0]]
+	# ptrentexp=[[fdate,0]]
+	# fbosell=[[fdate,0]]
+	# fbobuy=[[fdate,0]]
+	# wsbuy100=[[fdate,0]]
+	# wssell100=[[fdate,0]]
+	# wsbuyjet=[[fdate,0]]
+	# wsselljet=[[fdate,0]]
+	# wsbuybld=[[fdate,0]]
+	# wssellbld=[[fdate,0]]
+	# wsbuysupp=[[fdate,0]]
+	# wssellsupp=[[fdate,0]]
+	# grpay=[[fdate,0]]
+	
+	delta=datetime.timedelta(days=1)
+	i=0
+	print("Tallying daily payments from"+str(fdate[0])+"-"+str(fdate[1])+" to "+str(tdate[0])+"-"+str(tdate[1])+"...")
+	#(date text, to text, from text, amount real, reason text, location real, aircraft real)
+	while fdate <= todate:
+		fromdate=fdate+" 00:01"
+		todate=fdate+" 23:59"
+		if i>0 then:
+			for var in allthat:
+				var.add([fdate,var[i-1][1]])
+			# In case my understanding of variables has a fatal flaw
+			# rentexp.add([fdate,rentexp[i-1][1]])
+			# rentinc.add([fdate,rentinc[i-1][1]])
+			# assnmtexp.add([fdate,assnmtexp[i-1][1]])
+			# assnmtinc.add([fdate,assnmtinc[i-1][1]])
+			# pltfees.add([fdate,pltfees[i-1][1]])
+			# addcrewfee.add([fdate,addcrewfee[i-1][1]])
+			# gndcrewfee.add([fdate,gndcrewfee[i-1][1]])
+			# bkgfee.add([fdate,bkgfee[i-1][1]])
+			# ref100.add([fdate,ref100[i-1][1]])
+			# refjet.add([fdate,refjet[i-1][1]])
+			# mxexp.add([fdate,mxexp[i-1][1]])
+			# eqinstl.add([fdate,eqinstl[i-1][1]])
+			# acsold.add([fdate,acsold[i-1][1]])
+			# acbought.add([fdate,acbought[i-1][1]])
+			# fboref100.add([fdate,fboref100[i-1][1]])
+			# fborefjet.add([fdate,fborefjet[i-1][1]])
+			# fbogndcrew.add([fdate,fbogndcrew[i-1][1]])
+			# fborepinc.add([fdate,fborepinc[i-1][1]])
+			# fborepexp.add([fdate,fborepexp[i-1][1]])
+			# fboeqpexp.add([fdate,fboeqpexp[i-1][1]])
+			# fboeqpinc.add([fdate,fboeqpinc[i-1][1]])
+			# ptrentinc.add([fdate,ptrentinc[i-1][1]])
+			# ptrentexp.add([fdate,ptrentexp[i-1][1]])
+			# fbosell.add([fdate,fbosell[i-1][1]])
+			# fbobuy.add([fdate,fbobuy[i-1][1]])
+			# wsbuy100.add([fdate,wsbuy100[i-1][1]])
+			# wssell100.add([fdate,wssell100[i-1][1]])
+			# wsbuyjet.add([fdate,wsbuyjet[i-1][1]])
+			# wsselljet.add([fdate,wsselljet[i-1][1]])
+			# wsbuybld.add([fdate,wsbuybld[i-1][1]])
+			# wssellbld.add([fdate,wssellbld[i-1][1]])
+			# wsbuysupp.add([fdate,wsbuysupp[i-1][1]])
+			# wssellsupp.add([fdate,wssellsupp[i-1][1]])
+			# grpay.add([fdate,grpay[i-1][1]])
+
+		for payment in c.execute('SELECT * FROM payments WHERE date BETWEEN ? AND ?',(fromdate,todate)):
+			if payment[4]=="Rental of aircraft":
+				if payment[2]==user:
+					rentinc[i][1]+=payment[3]
+				else:
+					rentexp[i][1]+=payment[3]
+			elif payment[4]=="Pay for assignment":
+				if payment[2]==user:
+					assnmtinc[i][1]+=payment[3]
+				else:
+					assnmtexp[i][1]+=payment[3]
+			elif payment[4]=="Crew fee":
+				addcrewfee+=payment[3]
+			elif payment[4]=="FBO ground crew fee":
+				if payment[2]==user:
+					fbogndcrew[i][1]+=payment[3]
+				else:
+					gndcrewfee[i][1]+=payment[3]
+			elif payment[4]=="Booking Fee":
+				bkgfee[i][1]+=payment[3]
+			elif payment[4]=="Refuelling with JetA":
+				if payment[2]==user:
+					fborefjet[i][1]+=payment[3]
+				else:
+					refjet[i][1]+=payment[3]
+			elif payment[4]=="Refuelling with 100LL":
+				if payment[2]==user:
+					fboref100[i][1]+=payment[3]
+				else:
+					ref100[i][1]+=payment[3]
+			elif payment[4]=="Aircraft maintenance":
+				if payment[2]==user:
+					fborepinc[i][1]+=payment[3]
+				else:
+					mxexp[i][1]+=payment[3]
+			elif payment[4]=="Aircraft sale":
+				if payment[2]==user:
+					acsold[i][1]+=payment[3]
+				else:
+					acbought[i][1]+=payment[3]
+			elif payment[4]=="Sale of wholesale JetA":
+				if payment[2]==user:
+					wsselljet[i][1]+=payment[3]
+				else:
+					wsbuyjet[i][1]+=payment[3]
+			elif payment[4]=="Sale of wholesale 100LL":
+				if payment[2]==user:
+					wssell100[i][1]+=payment[3]
+				else:
+					wsbuy100[i][1]+=payment[3]
+			elif payment[4]=="Sale of supplies":
+				if payment[2]==user:
+					wssellsupp[i][1]+=payment[3]
+				else:
+					wsbuysupp[i][1]+=payment[3]
+			elif payment[4]=="Sale of building materials":
+				if payment[2]==user:
+					wssellbld[i][1]+=payment[3]
+				else:
+					wsbuybld[i][1]+=payment[3]
+			elif payment[4]=="Group payment":
+				if payment[2]==user:
+					grpay[i][1]+=payment[3]
+				else:
+					grpay[i][1]-=payment[3]
+			else:
+				print("No category found for "+payment[4])
+		fdate += delta
+		i += 1
+	
+	title="Money Stuff"
+	ylbl="Money"
+	print("Plotting figure for: "+title)
+	fig, ax = plt.subplots()
+	formatter=DateFormatter('%Y-%m-%d %H:%M')
+	ax.xaxis.set_major_formatter(formatter)
+	for data in allthat:
+		print("Attempting to plot the following "+str(len(data))+" dates:")
+		for date in [i[0] for i in data]:
+			print(date)
+		ax.plot([i[0] for i in data], [i[1] for i in data], 'o-')
+	fig.autofmt_xdate()
+	plt.title(title,fontsize=12)
+	plt.xlabel("Date")
+	plt.ylabel(ylbl)
+	plt.show()
+
+def sumpayments(conn,fdate,tdate):
+	c=getpaydbcon(conn)
+	#Income
+	rentinc=[0,"Rental income"]
+	assnmtinc=[0,"Assignment income"]
+	acsold=[0,"Aircraft sold"]
+	fboref100=[0,"100LL pumped"]
+	fborefjet=[0,"JetA pumped"]
+	fbogndcrew=[0,"Ground crew income"]
+	fborepinc[0,"Repair income"]
+	fboeqpinc[0,"Eqp instl income"]
+	ptrentinc=[0,"PT rent income"]
+	fbosell=[0,"FBO sold"]
+	wssell100=[0,"100LL sold"]
+	wsselljet=[0,"JetA sold"]
+	wssellbld=[0,"Building materials sold"]
+	wssellsupp=[0,"Supplies sold"]]
+	
+	#Expenses
+	rentexp=[0,"Rental expense"]
+	assnmtexp=[0,"Assignment expense"]
+	pltfee=[0,"Pilot fees"]
+	addcrewfee=[0,"Additional crew fee"]
+	gndcrewfee=[0,"Ground crew fee"]
+	bkgfee=[0,"Booking fee"]
+	ref100=[0,"100LL pumped"]
+	refjet=[0,"JetA pumped"]
+	mxexp=[0,"Maintenance"]
+	eqinstl=[0,"Equipment installed"]
+	acbought=[0,"Aircraft bought"]
+	fborepexp=[0,"FBO repair cost"]
+	fboeqpexp=[0,"FBO eqp instl"]
+	fbobuy=[0,"FBO bought"]
+	wsbuy100=[0,"100LL bought"]
+	wsbuyjet=[0,"JetA bought"]
+	wsbuybld=[0,"Building materials"]
+	wsbuysupp=[0,"Supplies"]
+	#rentexp, rentinc, assnmtexp, assnmtinc, pltfee, addcrewfee, gndcrewfee, bkgfee, ref100, refjet, mxexp, eqinstl, acsold, acbought, fboref100, fborefjet, fbogndcrew, fborepinc, fborepexp, fboeqpexp, fboeqpinc, ptrentinc, ptrentexp, fbosell, fbobuy, wsbuy100, wssell100, wsbuyjet, wsselljet, wsbuybld, wssellbld, wsbuysupp, wssellsupp, grpay=(0 for i in range(35))
+	# rentexp=0
+	# rentinc=0
+	# assnmtexp=0
+	# assnmtinc=0
+	# pltfee=0
+	# addcrewfee=0
+	# gndcrewfee=0
+	# bkgfee=0
+	# ref100=0
+	# refjet=0
+	# mxexp=0
+	# eqinstl=0
+	# acsold=0
+	# acbought=0
+	# fboref100=0
+	# fborefjet=0
+	# fbogndcrew=0
+	# fborepinc=0
+	# fborepexp=0
+	# fboeqpexp=0
+	# fboeqpinc=0
+	# ptrentinc=0
+	# ptrentexp=0
+	# fbosell=0
+	# fbobuy=0
+	# wsbuy100=0
+	# wssell100=0
+	# wsbuyjet=0
+	# wsselljet=0
+	# wsbuybld=0
+	# wssellbld=0
+	# wsbuysupp=0
+	# wssellsupp=0
+	# grpay=0
+	user=getname()
 	fromdate=fdate+" 00:01"
 	todate=tdate+" 23:59"
 	print("Tallying payments from"+str(fdate[0])+"-"+str(fdate[1])+" to "+str(tdate[0])+"-"+str(tdate[1])+"...")
 	#(date text, to text, from text, amount real, reason text, location real, aircraft real)
-	for payment in c.execute('SELECT * FROM payments WHERE date BETWEEN ? AND ? AND reason = "Rental of aircraft"', (fromdate,todate)):
-		rentexp+=payment[3]
-				
-	return listings
+	for payment in c.execute('SELECT * FROM payments WHERE date BETWEEN ? AND ?',(fromdate,todate)):
+		if payment[4]=="Rental of aircraft":
+			if payment[2]==user:
+				rentinc[0]+=payment[3]
+			else:
+				rentexp[0]+=payment[3]
+		elif payment[4]=="Pay for assignment":
+			if payment[2]==user:
+				assnmtinc[0]+=payment[3]
+			else:
+				assnmtexp[0]+=payment[3]
+		elif payment[4]=="Crew fee":
+			addcrewfee[0]+=payment[3]
+		elif payment[4]=="FBO ground crew fee":
+			if payment[2]==user:
+				fbogndcrew[0]+=payment[3]
+			else:
+				gndcrewfee[0]+=payment[3]
+		elif payment[4]=="Booking Fee":
+			bkgfee[0]+=payment[3]
+		elif payment[4]=="Refuelling with JetA":
+			if payment[2]==user:
+				fborefjet[0]+=payment[3]
+			else:
+				refjet[0]+=payment[3]
+		elif payment[4]=="Refuelling with 100LL":
+			if payment[2]==user:
+				fboref100[0]+=payment[3]
+			else:
+				ref100[0]+=payment[3]
+		elif payment[4]=="Aircraft maintenance":
+			if payment[2]==user:
+				fborepinc[0]+=payment[3]
+			else:
+				mxexp[0]+=payment[3]
+		elif payment[4]=="Aircraft sale":
+			if payment[2]==user:
+				acsold[0]+=payment[3]
+			else:
+				acbought[0]+=payment[3]
+		elif payment[4]=="Sale of wholesale JetA":
+			if payment[2]==user:
+				wsselljet[0]+=payment[3]
+			else:
+				wsbuyjet[0]+=payment[3]
+		elif payment[4]=="Sale of wholesale 100LL":
+			if payment[2]==user:
+				wssell100[0]+=payment[3]
+			else:
+				wsbuy100[0]+=payment[3]
+		elif payment[4]=="Sale of supplies":
+			if payment[2]==user:
+				wssellsupp[0]+=payment[3]
+			else:
+				wsbuysupp[0]+=payment[3]
+		elif payment[4]=="Sale of building materials":
+			if payment[2]==user:
+				wssellbld[0]+=payment[3]
+			else:
+				wsbuybld[0]+=payment[3]
+		elif payment[4]=="Group payment":
+			if payment[2]==user:
+				grpay[0]+=payment[3]
+			else:
+				grpay-=payment[3]
+		else:
+			print("No category found for "+payment[4])
+	#rentexp, rentinc, assnmtexp, assnmtinc, pltfee, addcrewfee, gndcrewfee, bkgfee, ref100, refjet, mxexp, eqinstl, acsold, acbought, fboref100, fborefjet, fbogndcrew, fborepinc, fborepexp, fboeqpexp, fboeqpinc, ptrentinc, ptrentexp, fbosell, fbobuy, wsbuy100, wssell100, wsbuyjet, wsselljet, wsbuybld, wssellbld, wsbuysupp, wssellsupp, grpay=(0 for i in range(35))
+
+	#Income nets
+	rent=[rentinc-rentexp,"Rental"]
+	assnmt=[assnmtinc-assnmtexp,"Assignments"]
+	ac=[acsold-acbought,"Aircraft"]
+	ws100=[fboref100+wssell100-wsbuy100,"WS 100LL"]
+	wsjet=[fborefjet+wsselljet-wsbuyjet,"WS JetA"]
+	fborep=[fborepinc-fborepexp,"FBO Repairs"]
+	fboeqp=[fboeqpinc-fboeqpexp,"FBO Eqp Instl"]
+	ptrent=[ptrentinc,"PT Rent"]
+	fbo=[fbosell-fbobuy,"FBO"]
+	sup=[wssellsupp-wsbuysupp,"Supplies"]
+	bld=[wssellbld-wsbuybld,"Building Mtrls"]
+	incnets=[rent, assnmt, ac, ws100, wsjet, fborep, fboeqp, ptrent, fbo, sup, bld]
+	
+	#Expense nets
+	#mxexp
+	#ref100
+	#refjet
+	#bkgfee
+	#gndcrewfee
+	#addcrewfee
+	#pltfee
+	
+	incnet=[]
+	expnet=[ref100, refjet, bkgfee, gndcrewfee, addcrewfee, pltfee, mxexp]
+	netinc=0
+	netexp=ref100[0]+refjet[0]+bkgfee[0]+gndcrewfee[0]+addcrewfee[0]+pltfee[0]+mxexp[0]
+	for net in incnets:
+		if net[0]>0:
+			incnet.add(net)
+			netinc+=net[0]
+		else:
+			expnet.add(net)
+			netexp+=net[0]
+	for net in incnet:
+		net[0]=net[0]/netinc*100
+	for net in expnet:
+		net[0]=net[0]/netinc*100
+	
+	rlabels = incnet[:][1]
+	elabels = expnet[:][1]
+	rsizes = incnet[:][0]
+	esizes = expnet[:][0]
+	
+	#Totals
+	revs=[rentinc, assnmtinc, acsold, fboref100, fborefjet, fbogndcrew, fborepinc, fboeqpinc, ptrentinc, fbosell, wssell100, wsselljet, wssellbld, wssellsupp]
+	exps=[rentexp, assnmtexp, pltfee, addcrewfee, gndcrewfee, bkgfee, ref100, refjet, mxexp, eqinstl, acbought, fborepexp, fboeqpexp, fbobuy, wsbuy100, wsbuyjet, wsbuybld, wsbuysupp]
+	rev=0
+	exp=0
+	for this in revs:
+		rev+=this[0]
+	for this in exps:
+		exp+=this[0]
+	for this in revs:
+		this[0]=this[0]/rev*100
+	for this in exps:
+		this[0]=this[0]/exp*100
+	revo=[0,"Other"]
+	expo=[0,"Other"]
+	revchart=[]
+	expchart=[]
+	for this in revs:
+		if this[0] < 10:
+			revo[0]+=this[0]
+		else:
+			revchart.add(this)
+	for this in exps:
+		if this[0] < 10:
+			expo[0]+=this[0]
+		else:
+			expchart.add(this)
+	revchart.add(revo)
+	expchart.add(expo)
+
+	# The slices will be ordered and plotted counter-clockwise.
+	#labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+	rlabels = revchart[:][1]
+	elabels = expchart[:][1]
+	#sizes = [15, 30, 45, 10]
+	rsizes = revchart[:][0]
+	esizes = expchart[:][0]
+	#colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+	rcolors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+	ecolors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+	#explode = (0, 0.1, 0, 0) # only "explode" the 2nd slice (i.e. 'Hogs') # But I don't wanna explode...
+
+	plt.pie(rsizes, labels=rlabels, colors=rcolors,
+			autopct='%1.1f%%', shadow=True, startangle=90)
+	plt.axis('equal') # Set aspect ratio to be equal so that pie is drawn as a circle.
+	plt.show()
+	plt.pie(esizes, labels=elabels, colors=ecolors,
+			autopct='%1.1f%%', shadow=True, startangle=90)
+	plt.axis('equal')
+	plt.show()
 
 def gettype(icao):
 	icaodict=dicts.getactypedict()
@@ -568,15 +946,15 @@ def main(argv):
 		logpaymonth(conn,year,month)
 	
 	if ppay==1:
-		fyear=fromdate.split('-', 2)[0]
-		fmonth=fromdate.split('-', 2)[1]
-		fday=fromdate.split('-', 2)[2]
-		tyear=todate.split('-', 2)[0]
-		tmonth=todate.split('-', 2)[1]
-		tday=todate.split('-', 2)[2]
-		fdate=(fyear,fmonth,fday)
-		tdate=(tyear,tmonth,tday)
-		plotpayments(conn,fdate,tdate)
+		# fyear=fromdate.split('-', 2)[0]
+		# fmonth=fromdate.split('-', 2)[1]
+		# fday=fromdate.split('-', 2)[2]
+		# tyear=todate.split('-', 2)[0]
+		# tmonth=todate.split('-', 2)[1]
+		# tday=todate.split('-', 2)[2]
+		# fdate=(fyear,fmonth,fday)
+		# tdate=(tyear,tmonth,tday)
+		plotpayments(conn,fromdate,todate)
 	
 	# We can also close the connection if we are done with it.
 	# Just be sure any changes have been committed or they will be lost. FOREVER
