9954fe8b8ec70bd9abd3b13216c44e62dfee556f
Nattgew <Nattgew@users.noreply.github.com>
2015-10-24 18:02:56 -0700

    Fix plotting issues

--- pricelog.py
+++ pricelog.py
@@ -472,22 +472,22 @@ def mapper(what, points, mincoords, maxcoords, title): # Put the points on a map
 	print("Mapping "+str(len(points))+" points...") #points is list of lists containing lat,lon, then possibly addtional data
 	print("min: "+str(mincoords[0])+","+str(mincoords[1])+"  max: "+str(maxcoords[0])+","+str(maxcoords[1]))
 	if maxcoords[1]-mincoords[1]>180 or maxcoords[0]-mincoords[0]>60: # Big spread, world with center aligned
-		print("Using world map")
+		#print("Using world map")
 		m = Basemap(projection='hammer', resolution=None, lon_0=(maxcoords[1]+mincoords[1])/2)
 	else: # Center map on area
-		print("Using regional map")
+		#print("Using regional map")
 		if len(points)>1:
 			width=maxcoords[1]-mincoords[1]
 			height=maxcoords[0]-mincoords[0]
 		else: # Provide good window for plotting one point
 			width=100
 			height=100
-		print("Width: "+str(width)+"  Height: "+str(height))
+		#print("Width: "+str(width)+"  Height: "+str(height))
 		llclo=dcoord(mincoords[1],-0.25*width,'lon')
 		llcla=dcoord(mincoords[0],-0.25*height,'lat')
 		urclo=dcoord(maxcoords[1],0.25*width,'lon')
 		urcla=dcoord(maxcoords[0],0.25*height,'lat')
-		print("ll="+str(llclo)+","+str(llcla)+"  ur="+str(urclo)+","+str(urcla))
+		#print("ll="+str(llclo)+","+str(llcla)+"  ur="+str(urclo)+","+str(urcla))
 		m = Basemap(projection='cyl', resolution=None, llcrnrlon=llclo, llcrnrlat=llcla, urcrnrlon=urclo, urcrnrlat=urcla)
 	if what=="ac":
 		if len(points) < 30: #Use awesome airplane symbol
@@ -520,6 +520,7 @@ def mapper(what, points, mincoords, maxcoords, title): # Put the points on a map
 			if pts[i]!=[[],[],[]]: #Check if any list has points
 				for j in range(3):
 					if pts[i][j]!=[]: #Check if this list has any points
+						print(pts[i][j])
 						x, y = m([k[1] for k in pts[i][j]], [k[0] for k in pts[i][j]]) #Populate points
 						if j==0: #Set color
 							c='#cc9900'
@@ -527,7 +528,11 @@ def mapper(what, points, mincoords, maxcoords, title): # Put the points on a map
 							c='b'
 						else:
 							c='k'
+						print(sz)
 						m.scatter(x,y,s=sz,marker='o',c=c) #Plot the points with these properties
+						for l in range(len(x)):
+							gals=int(round(i/2.65))
+							plt.text(x[l]-math.sqrt(sz/100),y[l],"~"+str(gals)+" gal",fontsize=7)
 						m.shadedrelief(scale=0.2)
 	plt.title(title,fontsize=12)
 	plt.show()
@@ -788,7 +793,7 @@ def getcommo(ctype): # Adds up locations and quantities of stuff to send to the 
 				else: #If location already added, then sum with other quantity
 					qty[i][1]+=item[2].split()
 					qty[i][2]=2 #Indicates a mix of t1 and t2
-			coords,cmin,cmax=getcoords(qty[:])
+			coords,cmin,cmax=getcoords(i[0] for i in qty)
 			if len(coords)==len(qty): #If not, there was some key error I guess
 				locations=[]
 				print("Working with "+str(len(coords))+" coords...")
@@ -847,6 +852,7 @@ def getcoords(data): #Get coordinates for a list of airports
 	latmax,lonmax,latmin,lonmin=100,200,100,200 #garbage to signal init
 	for row in data:
 		try:
+			print(row)
 			lat,lon=loc_dict[row]
 		except KeyError: #Probably "Airborne"
 			print("Key error on: "+str(row))
@@ -871,27 +877,53 @@ def getcoords(data): #Get coordinates for a list of airports
 	
 def plotdates(dlist,title,ylbl,sym,clr,save): #Plot a list of data vs. dates
 	if clr is None: #Allows for easy defaults I guess
-		clr=['b']
+		clr=['']
 	if sym is None:
 		sym=['-o']
 	print("Plotting figure for: "+title)
 	fig, ax = plt.subplots()
 	formatter=DateFormatter('%Y-%m-%d %H:%M')
 	ax.xaxis.set_major_formatter(formatter)
+	print(sym)
+	print(clr)
+	print(len(dlist))
 	i=0
-	ii=1 if len(sym)>1 else 0 #Changes whether each plot moves down a list of symbols/colors
+	ii=1 if len(sym)>2 else 0 #Changes whether each plot moves down a list of symbols/colors
 	j=0
-	jj=1 if len(clr)>1 else 0
-	#print("Iter i "+str(i)+" by "+str(ii)+"  j "+str(j)+" by "+str(jj))
+	jj=1 if len(clr)>2 else 0
 	for data in dlist:
+#		print("Iter i "+str(i)+" by "+str(ii)+"  j "+str(j)+" by "+str(jj))
+		if clr[j] is None:
+			clr[j]=''
 		if len(data[0])==2:
-			ax.plot([date2num(x[0]) for x in data], [x[1] for x in data], sym[i], c=clr[j])
+			#print("Plotting data with symbol "+sym[i]+" and color "+clr[j])
+			ax.plot([date2num(x[0]) for x in data], [x[1] for x in data], clr[j]+sym[i])
 		else:
 			ax.errorbar([date2num(x[0]) for x in data], [x[1] for x in data], yerr=[x[2] for x in data], fmt=sym[i], c=clr[j])
-		i+=ii if i<len(dlist)-2 else -i #Reference next symbol/color, if one is provided for each data entry
-		j+=jj if j<len(dlist)-2 else -j #Go back to zero if reaching length of data list
+		if i<len(dlist)-1: #Reference next symbol/color, if one is provided for each data entry
+			i+=ii
+		else: #Go back to zero if reaching length of data list
+			i-=i
+		if j<len(dlist)-1:
+			j+=jj
+		else:
+			j-=j
+		if len(dlist)>1 and data==dlist[-2]: #If only two elements given, second is for the last dataset
+			if len(sym)==2:
+				i=1
+			if len(clr)==2:
+				j=1
+	if dlist[-1][1][0]==getdtime("2100-12-31 23:59"): #Don't consider base price in range finding
+		del dlist[-1]
 	daterange=[min(min(date2num(i[0]) for i in j) for j in dlist),max(max(date2num(i[0]) for i in j) for j in dlist)]
-	pricerange=[min(min(i[1] for i in j) for j in dlist)*0.9,max(max(i[1] for i in j) for j in dlist)*1.1]
+	if isinstance(dlist[0][0][1], (list, tuple)):
+		minprice=min(min(i[1][0] for i in j) for j in dlist)
+		maxprice=max(max(i[1][0] for i in j) for j in dlist)
+	else:
+		minprice=min(min(i[1] for i in j) for j in dlist)
+		maxprice=max(max(i[1] for i in j) for j in dlist)
+	delta=maxprice-minprice
+	pricerange=[minprice-0.1*delta, maxprice+0.1*delta]
 	formatter=DateFormatter('%Y-%m-%d')
 	ax.xaxis.set_major_formatter(formatter)
 	fig.autofmt_xdate()
@@ -1245,7 +1277,7 @@ def main(argv): #This is where the magic happens
 			times=gettimeforsale(conn,timetype)
 			bprice=getbaseprice(timetype)
 			times.append([[getdtime("2014-01-01 00:01"),bprice],[getdtime("2100-12-31 23:59"),bprice]])
-			plotdates(times,"Prices of "+timetype,"Price",['o-'],['b'],0)
+			plotdates(times,"Prices of "+timetype,"Price",['o-','--'],[None,'r'],0)
 		if tot==1:
 			totals=gettotals(conn,tottype,fromdate,todate)
 			plotdates([totals],"Number of "+tottype+" for sale","Aircraft",['o-'],None,0)
