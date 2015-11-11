91846e4b880e637bec89d3e82700b32f630da2d5
Nattgew <Nattgew@users.noreply.github.com>
2015-11-11 09:34:38 -0800

    Use the workin script as a base now

--- mxnotify.py
+++ mxnotify.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-from xml.dom import minidom
-import urllib.request, math, smtplib
+import xml.etree.ElementTree as etree
+import urllib.request, math, smtplib, sys
 import fseutils # My custom FSE functions
 #import dicts # My script for custom dictionaries
 # import os, re, fileinput, csv, sqlite3
@@ -10,19 +10,23 @@ import fseutils # My custom FSE functions
 # from matplotlib.dates import DateFormatter, date2num
 # import matplotlib.pyplot as plt
 
-def getname(): #Returns username stored in file
-	with open('/mnt/data/XPLANE10/XSDK/mykey.txt', 'r') as f:
-		nothing = f.readline()
-		myname = f.readline()
-		myname=myname.strip()
-		return myname
-
 def getemail(): #Gets email info stored in file
-	with open('emailfile', 'r') as f:
+	with open('creds.txt', 'r') as f:
 		addr=f.readline().strip()
-		uname=f.readline().strip()
 		passw=f.readline().strip()
-		return addr,uname,passw
+		return addr,addr,passw
+
+def reldist(icao,rad): #Find distances of other airports from given airport
+	#print("Looking for airports near "+icao)
+	loc_dict=fseutils.build_csv("latlon")
+	clat,clon=loc_dict[icao]
+	dists=[]
+	for apt,coords in loc_dict.items():
+		if apt!=icao:
+			#print("Dist from "+str(clat)+" "+str(clon)+" to "+str(coords[0])+" "+str(coords[1]))
+			dist=fseutils.cosinedist(clat,clon,coords[0],coords[1])
+			dists.append((apt,dist))
+	return sorted(dists, key=lambda dist: dist[1])
 
 def getshops(icao):
 	services=fseutils.fserequest(1,'query=icao&search=fbo&icao='+icao,'FBO','xml')
@@ -31,70 +35,83 @@ def getshops(icao):
 		for opt in services:
 			thisfbo=fseutils.getbtns(opt, [("Status", 0), ("RepairShop", 0)])
 			if thisfbo==["Active", "Yes"]:
-				options.append(tuple(getbtns(opt, [("Name", 0), ("Owner", 0)])))
+				options.append(tuple(fseutils.getbtns(opt, [("Name", 0), ("Owner", 0)])))
 	return options
 
-def reldist(icao,rad): #Find distances of other airports from given airport
-	#print("Looking for airports near "+icao)
-	loc_dict=build_csv("latlon")
-	clat,clon=loc_dict[icao]
-	dists=[]
-	for apt,coords in loc_dict.items():
-		if apt!=icao:
-			#print("Dist from "+str(clat)+" "+str(clon)+" to "+str(coords[0])+" "+str(coords[1]))
-			dist=cosinedist(clat,clon,coords[0],coords[1])
-			dists.append((apt,dist))
-	return sorted(dists, key=lambda dist: dist[1])
+def isnew(needfixes):
+	oldnews=[]
+	fixed=[]
+	with open('aog.txt', 'r+') as f:
+		for aog in f:
+			for current in needfixes:
+				if current==aog:
+					oldnews.append(current)
+					break
+	with open('aog.txt', 'w') as f:
+		for current in needfixes:
+			f.write(current)
+	for oldie in oldnews:
+		needfixes.remove(oldie)
+	return needfixes
 
+ns = {'sfn': 'http://server.fseconomy.net'} #namespace for XML stuff
 aog=[] #List of planes and FBO options
 print("Sending request for aircraft list...")
 airplanes = fseutils.fserequest(1,'query=aircraft&search=key','Aircraft','xml')
 for plane in airplanes:
-	row=fseutils.getbtns(plane, [("NeedsRepair", 1), ("TimeLast100hr", 0)]) #Indications repair is needed
-	since100=int(row[1].split(":")[0])
+	nr=int(plane.find('sfn:NeedsRepair', ns).text) #Indications repair is needed
+	since100=int(plane.find('sfn:TimeLast100hr', ns).text.split(":")[0])
 	mx=0
-	if row[0]>0: #Needs repair
+	#print(str(nr)+" "+str(since100))
+	if nr>0:
 		mx=1
-	if since100>100: #100 hr past due
-		mx+=2
+	if since100>99: #100 hr past due
+		mx=2
 	if mx>0: #Something is broken
 		row=fseutils.getbtns(plane, [("Registration", 0), ("MakeModel", 0), ("Location", 0)]) #License and registration please
 		shops=getshops(row[2]) #Get list of shops here
 		if len(shops)==0: #Start looking around
 			relatives=reldist(row[2]) #List of all airports sorted by closest to this one
-			for neighbor in relatives
+			for neighbor in relatives:
 				shops=getshops(neighbor[0]) #Got any gwapes?
 				if len(shops)>0:
 					break
 		aog.append((row[0],row[1],row[2],mx,shops)) #Reg, Type, Loc, repair, options
+aog=isnew(aog)
+addr,uname,passw=getemail()
 msg="Airplanes in need of repair:"
-print(msg)
+#print(msg)
 if len(aog)>0:
 	for plane in aog:
 		if plane[3]==1:
 			repair="repair"
-		elif plane[3]==2:
-			repair="100-hour"
 		else:
-			repair="repair and 100-hr"
+			repair="100-hour"
 		out=plane[0]+"  "+plane[1]+" at "+plane[2]
 		msg+="\n"+out
-		print(out)
+		#print(out)
 		out="Needs "+repair+", options are:"
 		msg+="\n"+out
-		print(out)
+		#print(out)
 		for opt in plane[4]:
 			out=opt[0]+" owned by "+opt[1]
 			msg+="\n"+out
-			print(out)
+			#print(out)
 		msg+="\n"
-		print()
-	print(msg)
-	addr,uname,passw=getemail()
-	server=smtplib.SMTP('smtp.gmail.com:587')
-	server.starttls()
-	server.login(uname,passw)
-	server.sendmail(addr,addr,msg)
-	server.quit()
+		#print()
+	message="""\From: %s\nTo: %s\nSubject: FSE Aircraft Mx\n\n%s""" % (addr, addr, msg)
+	try:
+		server=smtplib.SMTP_SSL('smtp.gmail.com', 465)
+		server.ehlo()
+		server.login(uname,passw)
+		server.sendmail(addr,addr,message)
+		server.close()
+		print("Successfully sent the mail:")
+	except:
+		e = sys.exc_info()[0]
+		print("Failed to send the mail with error:")
+		print(e)
 else:
-	print("None")
+	msg+="\nNone"
+print()
+print(msg)
