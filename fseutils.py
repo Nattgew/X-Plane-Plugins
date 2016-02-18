f5f32bb5c2db337fe18419df6c8c093fe6451416
Nattgew <Nattgew@users.noreply.github.com>
2016-02-18 12:38:09 -0800

    Include email function here

--- fseutils.py
+++ fseutils.py
@@ -6,12 +6,37 @@ from datetime import datetime
 from mpl_toolkits.basemap import Basemap
 from matplotlib.dates import DateFormatter, date2num
 import matplotlib.pyplot as plt
+import smtplib, sys
 
 def getkey(): #Returns API key stored in file
 	with open('/mnt/data/XPLANE10/XSDK/mykey.txt', 'r') as f:
 		mykey = f.readline()
 		mykey=mykey.strip()
 		return mykey
+
+def getemail(): #Gets email info stored in file
+	with open('creds.txt', 'r') as f:
+		srvr=f.readline().strip()
+		addr=f.readline().strip()
+		passw=f.readline().strip()
+		addrto=f.readline().strip()
+		return srvr,addrto,addr,passw
+
+def sendemail(subj,msg): #Sends email
+	srvr,addrto,addr,passw=getemail()
+	message="""\From: %s\nTo: %s\nSubject: %s\n\n%s""" % (addr, addr, subj, msg)
+	try:
+		#print("Sending mail from "+addr+" to "+addrto)
+		server=smtplib.SMTP_SSL(srvr, 465)
+		server.ehlo()
+		server.login(addr,passw)
+		server.sendmail(addr,addrto,message)
+		server.close()
+		#print("Successfully sent the mail:")
+	except:
+		#e = sys.exc_info()[0]
+		#print("Failed to send the mail with error:")
+		#print(e)
 
 def fserequest(ra,rqst,tagname,fmt): #Requests data in format, returns list of requested tag
 	if ra==1:
